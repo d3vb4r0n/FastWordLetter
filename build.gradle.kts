c16c92339882fbaec251c66479d0ced5df3ca3d8
Maxim Pivitsin <maximpivitsin@MacBook-Air-Maxim.local>
2026-04-27 16:50:45 +0300

    added some dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     id("java")
+    id("application")
+    id("org.openjfx.javafxplugin") version "0.1.0"
 }
 
 group = "org.example"
@@ -9,10 +11,26 @@ repositories {
     mavenCentral()
 }
 
+
+javafx {
+    version = "21"
+    modules = listOf("javafx.controls")
+}
+
 dependencies {
+    implementation("org.apache.poi:poi-ooxml:5.4.1")
+
     testImplementation(platform("org.junit:junit-bom:6.0.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+application {
+    mainClass = "org.example.Main"
+}
+
+tasks.withType<JavaCompile>().configureEach {
+    options.encoding = "UTF-8"
 }
 
 tasks.test {
